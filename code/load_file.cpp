dd163c9499cce884bede07e91c9ddb129c07a907
d-a-t-s <dtamaro2023@udec.cl>
2025-07-03 21:14:25 -0400

    Cambio de la funcion load_file para normalizar el archivo de texto en el que se quiere buscar el patron

--- code/load_file.cpp
+++ code/load_file.cpp
@@ -6,6 +6,7 @@
 #include <string>
 #include <string_view>
 #include <filesystem>
+#include <cctype>
 
 using namespace std;
 namespace fs = filesystem;
@@ -16,12 +17,33 @@ string load_file(const string& file_name){
     fs::path directory = fs::path("../datasets/Concatenated") / file_name; //¿Esta bien si se le pasa asi el nombre del archivo considerando que se le esta pasando por referencia a la funcion?
 
     //Verificacion de que el archivo existe
-    ifstream input(directory, ios::in);
+    ifstream input(directory, ios::binary);
     if(!input){
         cerr << "Error: No se puedo abrir el archivo: " << directory << " en load_file.cpp" << endl;
         exit(1);
     }
 
     //Copia del archivo a un string
-    return string(istreambuf_iterator<char>(input), istreambuf_iterator<char>());
+    string result;
+    bool last_space = false;
+    char c;
+    while(input.get(c)){
+        if(c == '\n' || c == '\r' || c == '\t'){
+            c = ' ';
+        }
+
+        //Verificacion para espacios consecutivos
+        if(c == ' '){
+            if(last_space){
+                continue;
+            }
+            last_space = true;
+        }else{
+            last_space = false;
+        }
+
+        //Convertir a minúsculas
+        result += static_cast<char>(tolower(static_cast<unsigned char>(c)));
+    }
+    return result;
 }
